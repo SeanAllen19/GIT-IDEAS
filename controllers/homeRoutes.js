7f185967ed2c070fcc67b0185d0b24c18e785acf
re-gi <r.l.girndt@gmail.com>
2023-02-12 12:33:03 -0600

    homepage now has access to 10 most recent notes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,30 @@
 const router = require('express').Router();
-// const { User, Notes } = require('../models');
+const { User, Notes } = require('../models');
 // const withAuth = require('../utils/auth');
 
 // render homepage template
 router.get('/', async (req, res) => {
-    res.render('homepage');
+    try{
+        const notesData = await Notes.findAll({
+            limit: 10,
+            order: [
+                ['date_created', 'DESC']
+            ]
+        });
+
+        const notes = notesData.map((notes) => notes.get({ plain: true }));
+
+        res.render('homepage', {
+            notes,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
+
+router.get('/login', async (req, res) => {
+    res.render('login');
 });
 
 module.exports = router;
