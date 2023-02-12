e38b68d547e9d22c21ccec6ece466e6afb3fd010
re-gi <r.l.girndt@gmail.com>
2023-02-11 22:45:49 -0600

    "started practice axios/github query"

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 // const { User, Notes } = require('../models');
 // const withAuth = require('../utils/auth');
 
-// // render homepage template
-// router.get('/', async (req, res) => {
-//     res.render('homepage');
-// });
+// render homepage template
+router.get('/', async (req, res) => {
+    res.render('homepage');
+});
 
 module.exports = router;
