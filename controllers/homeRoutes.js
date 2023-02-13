f5cf3380a7ef8c8b9275d62ba43dbe79c1e01be6
re-gi <r.l.girndt@gmail.com>
2023-02-13 12:30:16 -0600

    search query seperated from results for fewer axios calls

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,6 +15,7 @@ try {
     const searchTerm = req.params.query;
     console.log(searchTerm);
 
+    resultObject = [];
     const queryStr = `https://api.github.com/search/repositories?q=${searchTerm}&per_page=10`;
     console.log(queryStr)
 
@@ -22,36 +23,39 @@ try {
 
 
     results.data.items.forEach((item) => {
-        resultObject.push({name: item.name});
+        resultObject.push({name: item.name, id: item.id});
     });
     console.log(resultObject)
 
-    res.render('searchResults', {
-        resultObject,
-        logged_in: req.session.logged_in
-    });
-
+    res.redirect('/results');
 } catch (err) {
     res.status(500).json({message: 'No good'})
 }
 })
 
-// render login template
-router.get('/login', async (req, res) => {
-    res.render('login');
+router.get('/results', async (req, res) => {
+    res.render('searchResults', {
+        resultObject,
+        logged_in: req.session.logged_in
+    });
 });
 
 // render newNote template from search result id
-router.get('/result/:id', async (req, res) => {
+router.get('/results/:id', async (req, res) => {
     
     const objectOne = resultObject[0];
     console.log('ObjectOne', objectOne);
-    
+
     res.render('newNote', {
         objectOne,
         user_id: req.session.user_id,
         logged_in: req.session.logged_in
     });
+});
+
+// render login template
+router.get('/login', async (req, res) => {
+    res.render('login');
 });
 
 // go to http://localhost:3001/tagmanager to see
