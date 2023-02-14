ee9da2d9b7c780ec2fcdbf7542c72f3700607d10
brettnachman <brettnachman@gmail.com>
2023-02-13 20:06:54 -0600

    js work

--- public/js/testgitCalls.js
+++ public/js/testgitCalls.js
@@ -3,10 +3,14 @@ event.preventDefault();
 
   const query = document.querySelector('#repo-search-input').value
   console.log(query)
-// if !query then query = null
-  document.location.replace(`/search/${query}`);
-}
 
+    if (!query) {
+    document.location.replace(`/search/javascript`)
+    }
+    else {
+    document.location.replace(`/search/${query}`);
+    }
+};
 document
   .querySelector('#repo-search-form')
   .addEventListener('submit', getRepoInfo);
