f6b0ded035dfed4b67199bd8993834eb63812e7a
Michael Smith <mdsmith02@gmail.com>
2023-02-14 21:30:37 -0600

    session timeout to 50min; youre to your

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const hbs = exphbs.create({ helpers });
 const sess = {
   secret: 'Super secret secret',
   cookie: {
-    maxAge: 300000,
+    maxAge: 3000000,
     httpOnly: true,
     secure: false,
     sameSite: 'strict',
