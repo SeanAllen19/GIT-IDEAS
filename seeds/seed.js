05a71b35e2c703222304e2f42ed3b1b6d54f4bfb
Michael Smith <mdsmith02@gmail.com>
2023-02-16 12:27:12 -0600

    comments and formatting fixes

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,41 +1,29 @@
-const sequelize = require('../config/connection');
-const { User, Notes, Tags } = require('../models');
+const sequelize = require("../config/connection");
+const { User, Notes, Tags } = require("../models");
 
-const userData = require('./userData.json');
-const notesData = require('./notesData.json');
-const tagsData = require('./tagsData.json')
-
+const userData = require("./userData.json");
+const notesData = require("./notesData.json");
+const tagsData = require("./tagsData.json");
 
 const seedDatabase = async () => {
-    await sequelize.sync({ force: true });
-  
-    const users = await User.bulkCreate(userData, {
-      individualHooks: true,
-      returning: true,
-    });
+  await sequelize.sync({ force: true });
 
-    const tSeeds = await Tags.bulkCreate(tagsData, {
-      individualHooks: true,
-      returning: true,
-    });
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
 
-    const nSeeds = await Notes.bulkCreate(notesData, {
-      individualHooks: true,
-      returning: true,
-    });
+  const tSeeds = await Tags.bulkCreate(tagsData, {
+    individualHooks: true,
+    returning: true,
+  });
 
+  const nSeeds = await Notes.bulkCreate(notesData, {
+    individualHooks: true,
+    returning: true,
+  });
 
+  process.exit(0);
+};
 
-
-  
-    // for (const notes of notesData) {
-    //   await Notes.create({
-    //     ...notes,
-    //     user_id: users[Math.floor(Math.random() * users.length)].id,
-    //   });
-    // }
-  
-    process.exit(0);
-  };
-  
-  seedDatabase();
+seedDatabase();
