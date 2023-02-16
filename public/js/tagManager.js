daf59659021e1f516aa209fbc80f4e9c9ba7e808
brettnachman <brettnachman@gmail.com>
2023-02-15 20:59:30 -0600

    Readme tweak. Comments added and old code deleted from public/js files

--- public/js/tagManager.js
+++ public/js/tagManager.js
@@ -10,28 +10,6 @@ const deleteTag = async (event) => {
   }
 };
 
-// const updateTag = async (event) => {
-   
-//   event.preventDefault();
-// //   console.log(event.target.dataset);
-// console.log("Updating tag....");
-//   const newTag = document.querySelector("#newTag-textarea").value.trim();
-//   console.log(newTag);
-
-//   const response = await axios.put(
-//     `/api/tags/${event.target.dataset.update_id}`,
-//     { title: newTag }
-//   );
-
-//   if ((response.statustext = "ok")) {
-//     console.log("Tag updated");
-//   } else {
-//     alert(response.statusText);
-//   }
-// };
-
 const deleteBtns = document.querySelectorAll(".btn-deleteTag");
-// const updateBtns = document.querySelectorAll(".form-updateTag");
 
 deleteBtns.forEach((button) => button.addEventListener("click", deleteTag));
-// updateBtns.forEach((button) => button.addEventListener("submit", updateTag));
