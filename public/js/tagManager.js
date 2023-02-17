b201d62ff78c32297e5d735ca9fc5258a008ec09
Michael Smith <mdsmith02@gmail.com>
2023-02-16 18:03:00 -0600

    edit and delete tags working, new layout

--- public/js/tagManager.js
+++ public/js/tagManager.js
@@ -1,15 +1,35 @@
-const deleteTag = async (event) => {
+// edit tag functionality
+const updateTag = async (event) => {
+    event.preventDefault();
+  const editTagText = event.target.previousElementSibling.value.trim();
+  const putTagBody = { title: editTagText}
+  
+  const response = await axios.put(
+    `/api/tags/${event.target.dataset.tagid}`, putTagBody);
 
+    if ((response.statustext = "OK")) {
+      document.location.replace("/tagmanager");
+    } else {
+      alert(response.statusText);
+    }
+};
+
+// delete tag functionality
+const deleteTag = async (event) => {
   const response = await axios.delete(
     `/api/tags/${event.target.dataset.tagid}`
   );
-  if (response.statustext = "OK") {
-    document.location.replace('/tagmanager');
+  if ((response.statustext = "OK")) {
+    document.location.replace("/tagmanager");
   } else {
     alert(response.statusText);
   }
 };
 
-const deleteBtns = document.querySelectorAll(".btn-deleteTag");
+// Add event listener to update buttons
+const updateBtns = document.querySelectorAll(".btn-updateTag");
+updateBtns.forEach((button) => button.addEventListener("click", updateTag));
 
+// Add event listener to delete buttons
+const deleteBtns = document.querySelectorAll(".btn-deleteTag");
 deleteBtns.forEach((button) => button.addEventListener("click", deleteTag));
